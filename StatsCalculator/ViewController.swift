e4eb87e33c13cd7f0bd2051236417a0ba05faf06
Bill Spell <william.spell@vanderbilt.edu>
2020-09-18 00:20:24 -0500

    Initial Commit

---
+++ StatsCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatsCalculator
+//
+//  Created by Kirby Bright on 9/18/20.
+//  Copyright © 2020 William Spell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
